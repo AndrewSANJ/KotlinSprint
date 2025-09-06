a216ea1714f395df786640eb30ce2e9784f58182
Andrey S <hawaiisan@yandex.ru>
2025-09-06 07:08:30 +0300

    KS-2-4 mined

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val crystalOreMined = 7
+    val ironOreMined = 11
+
+    val buffMined = 20
+
+    println("Additional crystal ore mined ${crystalOreMined * buffMined / 100}")
+    println("Additional iron ore mined ${ironOreMined * buffMined / 100}")
+
+}
