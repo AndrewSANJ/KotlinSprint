8d8cb9a0d529277bd1d04bea6e9e12ab035cacd7
Andrey S <hawaiisan@yandex.ru>
2025-09-07 06:24:17 +0300

    KS-3-2 woman change family
    Signed-off-by: Субач <opolontrader@mail.ru>

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val lastname1 = "Андреева"
+    val firstname = "Татьяна"
+    val midname = "Сергеевна"
+    val age1 = 20
+
+    val lastname2 = "Сидорова"
+    val age2 = 22
+
+    println("$lastname1 $firstname $midname $age1 лет")
+    println("$lastname2 $firstname $midname $age2 года")
+}
