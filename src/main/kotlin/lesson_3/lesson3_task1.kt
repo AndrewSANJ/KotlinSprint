1cfb9915da78b2da10199b515d4af67f03050543
Andrey S <hawaiisan@yandex.ru>
2025-09-07 06:13:26 +0300

    KS-3-1 welcome string
    Signed-off-by: Субач <opolontrader@mail.ru>

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val welcome1 = "Добрый день"
+    val welcome2 = "Добрый вечер"
+    val name = "Иван"
+
+    println("$welcome1 $name")
+    println("$welcome2 $name")
+}
