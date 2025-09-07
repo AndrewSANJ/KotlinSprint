73c0c455d1bf1d95c18f5228255b4828279c1b6f
Andrey S <hawaiisan@yandex.ru>
2025-09-07 18:19:26 +0300

    main 2

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,14 @@
+
+const val MINUTES_AN_HOUR = 60
+const val SECONDS_AN_MINUTE = 60
+
+fun main() {
+
+    val second = 6480
+    val hours = second / (MINUTES_AN_HOUR * SECONDS_AN_MINUTE)
+    val minutes = (second - hours * MINUTES_AN_HOUR * SECONDS_AN_MINUTE) / SECONDS_AN_MINUTE
+    val seconds = second % SECONDS_AN_MINUTE
+
+    print("%02d:%02d:%02d".format(hours, minutes, seconds))
+
+}
