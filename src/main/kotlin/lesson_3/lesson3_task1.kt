a072cabbbb8c486b1f12ba0a12be994adb5e7192
Andrey S <hawaiisan@yandex.ru>
2025-09-08 19:23:56 +0300

    KS-3-1 adjusted

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,10 +2,13 @@ package org.example.lesson_3
 
 fun main() {
 
-    val welcome1 = "Добрый день"
-    val welcome2 = "Добрый вечер"
     val name = "Иван"
 
-    println("$welcome1 $name")
-    println("$welcome2 $name")
+    var welcome = "Добрый день"
+
+    println("$welcome $name")
+
+    welcome = "Добрый вечер"
+
+    println("$welcome $name")
 }
