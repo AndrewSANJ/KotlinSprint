c1ddccf001d5a4ff7073f8f770c3d5abc3d8c14e
Andrey S <hawaiisan@yandex.ru>
2025-09-08 19:26:57 +0300

    KS-3-2 adjusted

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -2,14 +2,15 @@ package org.example.lesson_3
 
 fun main() {
 
-    val lastname1 = "Андреева"
+    var lastname = "Андреева"
     val firstname = "Татьяна"
     val midname = "Сергеевна"
     val age1 = 20
 
-    val lastname2 = "Сидорова"
+    println("$lastname $firstname $midname $age1 лет")
+
+    lastname = "Сидорова"
     val age2 = 22
 
-    println("$lastname1 $firstname $midname $age1 лет")
-    println("$lastname2 $firstname $midname $age2 года")
+    println("$lastname $firstname $midname $age2 года")
 }
